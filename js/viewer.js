d3b74277cf8299d99b82bf9a131fd9a9e6f992b0
Heleri Koltsin <kolchinheleri@gmail.com>
2026-07-10 01:00:24 +0200

    Frame exhibition on load

--- js/viewer.js
+++ js/viewer.js
@@ -1,11 +1,41 @@
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { camera, renderer, scene } from "./scene.js";
+import { objects } from "./loader.js";
 
 export const orbit = new OrbitControls(camera, renderer.domElement);
 orbit.enableDamping = true;
-orbit.target.set(0, 0, 0);
+orbit.target.set(0, 0.8, 0);
+
+function frameObjects() {
+  if (!objects.length) return;
+
+  const box = new THREE.Box3();
+
+  objects.forEach((object) => {
+    box.expandByObject(object);
+  });
+
+  const center = box.getCenter(new THREE.Vector3());
+  const size = box.getSize(new THREE.Vector3());
+
+  const maxDim = Math.max(size.x, size.y, size.z);
+  const distance = maxDim * 2.2;
+
+  camera.position.set(
+    center.x,
+    center.y + maxDim * 0.45,
+    center.z + distance
+  );
+
+  camera.lookAt(center);
+  orbit.target.copy(center);
+  orbit.update();
+}
 
 export function startViewer() {
+  setTimeout(frameObjects, 800);
+
   function animate() {
     requestAnimationFrame(animate);
     orbit.update();
@@ -19,4 +49,6 @@ window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
+
+  setTimeout(frameObjects, 200);
 });
