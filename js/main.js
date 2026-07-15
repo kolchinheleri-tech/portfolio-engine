e019ac8f4fe00f0111be8f57181f1b205e3aef4a
Heleri Koltsin <kolchinheleri@gmail.com>
2026-07-15 20:10:20 +0200

    fix: center exhibition on page load

--- js/main.js
+++ js/main.js
@@ -8,10 +8,6 @@ import {
   orbit
 } from "./viewer.js";
 
-import {
-  camera
-} from "./scene.js";
-
 import "./transform.js";
 
 import {
@@ -84,34 +80,16 @@ function configureAdminInterface() {
 
 startViewer();
 
-loadModels((savedComposition) => {
-  const savedCamera =
-    savedComposition?.camera;
-
-  if (
-    savedCamera?.position &&
-    savedCamera?.target
-  ) {
-    camera.position.set(
-      savedCamera.position.x,
-      savedCamera.position.y,
-      savedCamera.position.z
-    );
-
-    orbit.target.set(
-      savedCamera.target.x,
-      savedCamera.target.y,
-      savedCamera.target.z
-    );
-
-    camera.lookAt(
-      orbit.target
-    );
-
-    orbit.update();
-  } else {
-    frameObjects();
-  }
+loadModels(() => {
+  /*
+   * Kadreerime kõik objektid automaatselt.
+   *
+   * Salvestatud kompositsiooni kaamera asendit
+   * ei kasutata, sest see võib olla liiga paremal,
+   * liiga lähedal või liiga kaugel.
+   */
+  frameObjects();
+  orbit.update();
 
   const loading =
     document.getElementById(
